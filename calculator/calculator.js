8b4829760e17737c41abeeba202a739854a1b392
Jessica DelVecchio <hey@jdvdv.com>
2024-09-17 16:16:42 -0700

    finished calculator. checked against solution. minor updates

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -10,41 +10,23 @@ window.addEventListener("DOMContentLoaded", function () {
 });
 
 function getCurrentUIValues() {
-  // make sure that all fields are filled and contain numbers
-  // if (
-  //   Number.isNAN(values.amount) ||
-  //   Number.isNAN(values.years) ||
-  //   Number.isNAN(values.rate)
-  // ) {
-  //   alert("Only numbers are accepted.");
-  // }
-  // if (
-  //   amountUI.value.trim() === "" ||
-  //   yearsUI.value.trim() === "" ||
-  //   rateUI.value.trim() === ""
-  // ) {
-  //   alert("All fields are necessary to continue.");
-  // } else {
   return {
     amount: +document.getElementById("loan-amount").value,
     years: +document.getElementById("loan-years").value,
     rate: +document.getElementById("loan-rate").value,
   };
-  // }
 }
 
 // ****** This function sets up default values that will appear on page load.
 function setupIntialValues() {
   // sets the following default values in the DOM
-  // // Put some default values in the inputs
   const values = {
-    amount: 1000,
-    years: 10,
-    rate: 5,
+    amount: 10000,
+    years: 8,
+    rate: 5.8,
   };
 
   // variables reflect above default values from the DOM
-  // // Get the inputs from the DOM.
   const amountUI = document.getElementById("loan-amount");
   amountUI.value = values.amount;
 
@@ -55,45 +37,31 @@ function setupIntialValues() {
   rateUI.value = values.rate;
 
   // function `calculateMonthlyPayment(values);` uses above set default values
-  // // Call a function to calculate the current monthly payment
-  calculateMonthlyPayment(values);
+  update();
 }
 
-// Get the current values from the UI
-// Update the monthly payment
-function update() {}
+// ****** Get the current values from the UI Update the monthly payment
+function update() {
+  // take current UI values and calculate monthly payment
+  const currentUIValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentUIValues));
+}
 
 // ****** Given an object of values (a value has amount, years and rate ) calculate the monthly payment
-
 function calculateMonthlyPayment(values) {
-  // declaring our variables
-  values.amount; // principal
-  values.years;
-  values.rate;
-
-  console.log(values.amount);
-  console.log(values.years);
-  console.log(values.rate);
-
   // convert our values to use in monthly payment calculation
-  let i = values.rate / 12; // periodic interest rate (in our case yearly rate ÷ 12)
-  let n = values.years * 12; // total number of payments (years × 12)
+  let i = values.rate / 100 / 12; // periodic interest rate (in our case yearly rate ÷ 12)
+  let n = Math.floor(values.years * 12); // total number of payments (years × 12)
 
-  console.log(i);
-  console.log(n);
-
-  // calculate the monthly payment.  The output should be a string
-  let monthly = (values.amount * i) / (1 - Math.pow(1 + i, -n));
-
-  console.log("monthly", monthly.toFixed(2));
-
-  // that always has 2 decimal places.
-  return monthly.toFixed(2);
+  // calculate the monthly payment.  The output should be a string, to 2 decimal points
+  return ((values.amount * i) / (1 - Math.pow(1 + i, -n))).toFixed(2);
 }
 
-// Given a string representing the monthly payment value,
-// update the UI to show the value.
+// ****** Given a string representing the monthly payment value, update the UI to show the value.
 function updateMonthly(monthly) {
+  // define the variable for monthly payment in DOM
   const monthlyUI = document.getElementById("monthly-payment");
-  monthlyUI.innerText = monthly;
+
+  // update DOM with monthly payment string
+  monthlyUI.innerText = "$" + monthly;
 }
